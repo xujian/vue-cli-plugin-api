d70e68e88d21fa7fb069d501b6afac9554b4381b
xujian <782135@qq.com>
2018-09-06 11:18:17 +0800

    fix eslint problems

--- generator/templates/plugins/http/index.js
+++ generator/templates/plugins/http/index.js
@@ -1,17 +1,15 @@
 import Vue from 'vue'
 import http from 'vue-http'
-import mappings from './mappings'
 
-Plugin.install = function(Vue, options) {
+Plugin.install = function (Vue, options) {
   Object.defineProperties(Vue.prototype, {
     $http: {
-      get() {
+      get () {
         return http
       }
     }
-  });
-};
+  })
+}
 
 Vue.use(Plugin)
-
-export default Plugin;
+export default Plugin
